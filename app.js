69abaaceb7ff3f50c8dc9f5ff4775f8bd55157ab
silverframe <stefanieseah@Stefanies-MacBook-Pro.local>
2016-03-02 15:05:15 +0800

    new function

--- app.js
+++ app.js
@@ -1,189 +1,215 @@
 /*global $*/
-$(function () {
-    'use strict';
-		getNames();
-		createGrid();
-    myBoats("carrier");
-	  myBoats("battleship");
-    myBoats("destroyer");
-	  myBoats("submarine");
-	  myBoats("patrolboat");
+var turn = "X";
 
-	function myBoats(boats) {
-		var add = 0;
-		if (boats == "carrier") {
-			add = 4;
-		} else if (boats == "battleship") {
-			add = 3;
-		} else if (boats == "destroyer") {
-			add = 2;
-		} else if (boats == "submarine") {
-			add = 2;
-		} else if (boats == "patrolboat") {
-			add = 1;
-		}
-		$("#" + boats).on('click', function() {
+var counter = 0;
 
-			$(".square").hover(function (event) {
-			  var dataX = event.delegateTarget.dataset.x;
-				var dataY = event.delegateTarget.dataset.y;
-				$(this).toggleClass('hover-ship');
-				for (var i = 0; i < add; i++) {
-					var query = '.square[data-x="' + dataX + '"]' + '[data-y="' + ((Number(dataY)) + (i + 1)) + '"]';
-					var $cell = $(query);
-					$cell.toggleClass('hover-ship');
+var boats = ["patrolboat", "submarine", "carrier", "destroyer", "battleship"]
 
-				};
-				$(this).on('click', function() {
-					$(this).addClass('place-ship');
-					for (var i = 0; i < add; i++) {
-						var query = '.square[data-x="' + dataX + '"]' + '[data-y="' + ((Number(dataY)) + (i + 1)) + '"]';
-						var $cell = $(query);
-						$cell.addClass('place-ship');
-					}
-				})
+var placedBoats =[ ];
+
+$(function() {
+  'use strict';
+	createGrid();
+	getNames();
+	setShips();
 })
 
-			$(".cell").hover(function(event) {
-				// console.log(event.delegateTarget.dataset.x);
-				// console.log(event.delegateTarget.dataset.y);
-				var dataX = event.delegateTarget.dataset.x;
-				var dataY = event.delegateTarget.dataset.y;
-				$(this).toggleClass('hover-ship');
-				for (var i = 0; i < add; i++) {
-					var query = '.cell[data-x="' + dataX + '"]' + '[data-y="' + ((Number(dataY)) + (i + 1)) + '"]';
-					// var next = '.cell[data-x="' + dataX + '"]' + '[data-y="' + (Number(dataY) + add) + '"]';
-					var $cell = $(query);
-					$cell.toggleClass('hover-ship');
-				}
+function getNames() {
 
-				$(this).on('click', function() {
-					$(this).addClass('place-ship');
-					for (var i = 0; i < add; i++) {
-						var query = '.cell[data-x="' + dataX + '"]' + '[data-y="' + ((Number(dataY)) + (i + 1)) + '"]';
-						var $cell = $(query);
-						$cell.addClass('place-ship');
-					}
-				})
-			})
+  var qn1 = prompt("Please enter your name", "Player 1");
+  var qn2 = prompt("Please enter your name", "Player 2");
+
+  if (qn1 !== null && qn2 !== null) {
+    console.log($('#player1'));
+    $("#player1").text(qn1 + "'s Board");
+    $("#enemyHitsHeader").text(qn1 + "'s Hits");
+    $("#enemyMissesHeader").text(qn1 + "'s Misses");
+    $("#player2").text(qn2 + "'s Board");
+    $("#hitsHeader").text(qn2 + "'s Hits");
+    $("#missedHitsHeader").text(qn2 + "'s Misses");
+  }
+
+}
+
+function setShips () {
+	// alert("Please set the ships");
+	myBoats("carrier");
+  myBoats("battleship");
+  myBoats("destroyer");
+  myBoats("submarine");
+  myBoats("patrolboat");
+}
+
+function myBoats(boats) {
+  var add = 0;
+  var allBoats = ["patrolboat", "submarine", "carrier", "destroyer", "battleship"]
+  if (boats == "carrier") {
+    add = 4;
+  } else if (boats == "battleship") {
+    add = 3;
+  } else if (boats == "destroyer") {
+    add = 2;
+  } else if (boats == "submarine") {
+    add = 2;
+  } else if (boats == "patrolboat") {
+    add = 1;
+  }
+
+  $("#" + boats).on('click', function() {
+
+    $(".square").hover(function(event) {
+      var dataX = event.delegateTarget.dataset.x;
+      var dataY = event.delegateTarget.dataset.y;
+      $(this).toggleClass('hover-ship');
+      for (var i = 0; i < add; i++) {
+        var query = '.square[data-x="' + dataX + '"]' + '[data-y="' + ((Number(dataY)) + (i + 1)) + '"]';
+        var $cell = $(query);
+        $cell.toggleClass('hover-ship');
+
+      };
+
+			// $("#rotate-button").click(function() {
+			// 	$(this).toggleClass('hover-ship');
+			// 	for (var i = 0; i < add; i++) {
+			// 		var query = '.square[data-x="' + ((Number(dataX)) + (i + 1)) + '"]' + '[data-y="' + dataY + '"]';
+			// 		var $cell = $(query);
+			// 		$cell.toggleClass('hover-ship'); }
+			// 	});
+
+
+      $(this).on('click', function() {
+        $(this).addClass('place-ship');
+        var x = Number($(this).attr('data-x'));
+        var y = Number($(this).attr('data-y'));
+        arrayShips[x][y] = true;
+        for (var i = 0; i < add; i++) {
+          var query = '.square[data-x="' + dataX + '"]' + '[data-y="' + ((Number(dataY)) + (i + 1)) + '"]';
+          var $cell = $(query);
+          var x = Number($cell.attr('data-x'));
+          var y = Number($cell.attr('data-y'));
+          arrayShips[x][y] = true;
+          $cell.addClass('place-ship');
+          // console.log(arrayShips);
+        }
+
+      })
+    })
+
+    $(".cell").hover(function(event) {
+      var dataX = event.delegateTarget.dataset.x;
+      var dataY = event.delegateTarget.dataset.y;
+      $(this).toggleClass('hover-ship');
+      for (var i = 0; i < add; i++) {
+        var query = '.cell[data-x="' + dataX + '"]' + '[data-y="' + ((Number(dataY)) + (i + 1)) + '"]';
+        var $cell = $(query);
+        $cell.toggleClass('hover-ship');
+      }
+
+      $(this).on('click', function() {
+        $(this).addClass('place-ship');
+        var x = Number($(this).attr('data-x'));
+        var y = Number($(this).attr('data-y'));
+        arrayShips[x][y] = true;
+        for (var i = 0; i < add; i++) {
+          var query = '.cell[data-x="' + dataX + '"]' + '[data-y="' + ((Number(dataY)) + (i + 1)) + '"]';
+          var $cell = $(query);
+          var x = Number($cell.attr('data-x'));
+          var y = Number($cell.attr('data-y'));
+          arrayShips[x][y] = true;
+          $cell.addClass('place-ship');
+          // console.log(arrayShips);
+
+        }
+      })
+    })
+  })
+}
+
+function rotate () {
+	$('#rotate-button').click(function(){
+		$(this).toggleClass('hover-ship');
+		for (var i = 0; i < add; i++) {
+			var query = '.square[data-x="' + ((Number(dataX)) + (i + 1)) + '"]' + '[data-y="' + dataY + '"]';
+			var $cell = $(query);
+			$cell.toggleClass('hover-ship'); }
 		})
-}})
+	}
 
 
 
+var arrayShips = createShipsArray();
+
+function createGrid() {
+  var numberGrid1 = $('.cell');
+  var numberGrid2 = $('.square');
+  var count = 0;
+  var count1 = 0;
+
+  for (var i = 0; i < 10; i++) {
+    for (var j = 0; j < 10; j++) {
+      numberGrid1[count].setAttribute('data-x', i);
+      numberGrid1[count].setAttribute('data-y', j);
+      count++;
+
+    }
+  }
+
+  for (var i = 0; i < 10; i++) {
+    for (var j = 0; j < 10; j++) {
+      numberGrid2[count1].setAttribute('data-x', i);
+      numberGrid2[count1].setAttribute('data-y', j);
+      count1++;
+
+    }
+  }
+}
+
+function createShipsArray() {
+  var outerArray = [];
+  for (var i = 0; i < 10; i++) {
+    var innerArray = []
+    for (var j = 0; j < 10; j++) {
+      innerArray.push(false);
+    }
+    outerArray.push(innerArray)
+  }
+  return outerArray;
+}
 
 
-		var arrayShips = createShipsArray();
+function changeColor() {
+  $(".square").click(function() {
+    $(this).text("X");
+    $(this).addClass('fire');
+  });
+}
 
-		$('.square').on('click', function() {
-			var x = $(this).attr("data-x");
-			var y = $(this).attr("data-y");
-			arrayShips[x][y] = true;
-			// console.log(arrayShips);
+// function placeShip() {
+//   $('.cell').on('click', function() {
+//     var x = Number($(this).attr('data-x'));
+//     var y = Number($(this).attr('data-y'));
+//     arrayShips[x][y] = true;
+//     console.log(arrayShips[x][y]);
+//   })
+// }
 
-		});
+function startTimer(duration, display) {
+  var timer = duration,
+    seconds;
+  setInterval(function() {
+    seconds = parseInt(timer % 60, 10);
+    seconds = seconds < 10 ? "0" + seconds : seconds;
 
-		// var arrayShips = [[]];
-		function createShipsArray() {
-			var outerArray = [];
+    display.textContent = seconds;
 
-			for (var i = 0; i < 10; i++) {
-				var innerArray = []
-				for (var j = 0; j < 10; j++) {
-					innerArray.push(false);
-				}
-				outerArray.push(innerArray)
-			}
-			return outerArray;
-		}
+    if (--timer < 0) {
+      timer = duration;
+    }
+  }, 1000)
+};
 
-
-		function getNames() {
-
-			var qn1 = prompt("Please enter your name", "Player 1");
-			var qn2 = prompt("Please enter your name", "Player 2");
-
-			if (qn1 !== null && qn2 !== null) {
-				console.log($('#player1'));
-				$("#player1").text(qn1 + "'s Board");
-				$("#enemyHitsHeader").text(qn1 + "'s Hits");
-				$("#enemyMissesHeader").text(qn1 + "'s Misses");
-				$("#player2").text(qn2 + "'s Board");
-				$("#hitsHeader").text(qn2 + "'s Hits");
-				$("#missedHitsHeader").text(qn2 + "'s Misses");
-				// console.log("welcome " + qn1);
-
-			}
-
-			// changeColor ();
-		}
-
-
-		function changeColor() {
-			$(".square").click(function() {
-				$(this).text("X");
-				$(this).addClass('fire');
-			});
-		}
-
-		$('.squares').on('click', function() {
-			var x = $(this).data - x;
-			var y = $(this).data - y;
-			arrayShips[x][y] = true;
-			// console.log(x);
-			// console.log(y);
-		})
-		// console.log(outerArray)
-		//
-		// for (var i = 0; i < 10 ; i++) {
-		//   var innerArray = []
-		//   for (var j = 0; j < 10; j++) {
-		//     innerArray.push(0)
-		//   }
-		//   outerArray.push(innerArray)
-		// }
-
-		function createGrid() {
-			var numberGrid1 = $('.cell');
-			var numberGrid2 = $('.square');
-			var count = 0;
-			var count1 = 0;
-
-			for (var i = 0; i < 10; i++) {
-				for (var j = 0; j < 10; j++) {
-					numberGrid1[count].setAttribute('data-x', i);
-					numberGrid1[count].setAttribute('data-y', j);
-					count++;
-
-				}
-			}
-
-			for (var i = 0; i < 10; i++) {
-				for (var j = 0; j < 10; j++) {
-					numberGrid2[count1].setAttribute('data-x', i);
-					numberGrid2[count1].setAttribute('data-y', j);
-					count1++;
-
-				}
-			}
-		}
-
-	// function startTimer(duration, display) {
-	//     var timer = duration, seconds;
-	//     setInterval(function () {
-	//         seconds = parseInt(timer % 60, 10);
-	//         seconds = seconds < 10 ? "0" + seconds : seconds;
-	//
-	//         display.textContent = seconds;
-	//
-	//         if (--timer < 0) {
-	//             timer = duration;
-	//         }
-	//     }, 1000)
-	// } ;
-	//
-	// window.onload = function () {
-	//     var tenSeconds = 10,
-	//         display = document.querySelector('#countdownTimer');
-	//     startTimer(tenSeconds, display);
-	// } ;
+window.onload = function() {
+  var tenSeconds = 10,
+    display = document.querySelector('#countdownTimer');
+  startTimer(tenSeconds, display);
+};
